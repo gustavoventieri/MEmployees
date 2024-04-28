aeb08e1d271e1e625d63d6f9fbc56df04df0aa02
gustavoventieri <ventierigustavo@gmail.com>
2024-04-27 23:59:37 -0300

    feat: initial config

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import "dotenv/config"
+import { server } from './server/Server';
+const port = process.env.PORT
+
+server.listen(port, () => console.log(`Servidor online e rodando na porta ${port}`))
