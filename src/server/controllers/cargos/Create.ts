e49a5fc2acfb5af96b3928d774ddb7a6049da717
gustavoventieri <ventierigustavo@gmail.com>
2024-04-30 00:23:33 -0300

    fix: improved jest setup to auto migrate knex database

--- src/server/controllers/cargos/Create.ts
+++ src/server/controllers/cargos/Create.ts
@@ -3,17 +3,28 @@ import { StatusCodes } from "http-status-codes";
 import * as yup from "yup";
 import { validation } from "../../shared/middlewares";
 import { ICargo } from "../../database/models";
+import { CargoProviders } from "../../database/providers/cargos";
 
 interface IBodyProps extends Omit<ICargo, "id"> {}
 
 export const createValidation = validation((getSchema) => ({
   body: getSchema<IBodyProps>(
     yup.object().shape({
-      nome: yup.string().required().min(3),
+      nome: yup.string().required().max(150).min(3),
     })
   ),
 }));
 
 export const create = async (req: Request<{}, {}, ICargo>, res: Response) => {
-  return res.status(StatusCodes.CREATED).json(1);
+  const result = await CargoProviders.create(req.body);
+
+  if (result instanceof Error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: {
+        default: result.message,
+      },
+    });
+  }
+
+  return res.status(StatusCodes.CREATED).json(result);
 };
---
+++ src/server/database/providers/cargos/index.ts
@@ -0,0 +1,13 @@
+//import * as deleteById from "./Delete";
+//import * as updateById from "./Update";
+//import * as getById from "./GetById";
+import * as create from "./Create";
+//import * as getAll from "./GetAll";
+
+export const CargoProviders = {
+  //...deleteById,
+  //...updateById,
+  //...getById,
+  ...create,
+  //...getAll,
+};
