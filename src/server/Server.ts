71fcff9530f1cc91e493298f15b6cec3e7baea61
gustavoventieri <ventierigustavo@gmail.com>
2024-06-09 02:24:09 -0300

    feat: added cors configuration

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,8 +1,13 @@
 import "dotenv/config";
+import cors from "cors";
 import "./shared/services/TranslationYup";
 import express from "express";
 import { router } from "./routes";
 const server = express();
+
+server.use(cors({
+  origin: process.env.ENABLED_CORS?.split(';') || []
+}));
 server.use(express.json());
 
 server.use(router);
