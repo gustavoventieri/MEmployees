ac918f3be0ab6823279f81e16118850bf154a96c
gustavoventieri <ventierigustavo@gmail.com>
2024-11-18 02:40:40 -0300

    feat: dotenv and structure of folders updated

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,6 @@
 import { server } from "./server/Server";
 
 // Iniciando o servidor
-server.listen(process.env.PORT || 8080, () => console.log(`Servidor Rodando na Porta ${process.env.PORT || 8080}`)
+server.listen(process.env.PORT || 8080, () =>
+  console.log(`Servidor Rodando na Porta ${process.env.PORT || 8080}`)
 );
--- api/src/server/Server.ts
+++ api/src/server/Server.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { router } from "./routes";
 import "dotenv/config";
+
 // Instanciando o servidor
 const server = express();
+
 // Utiliando as rotas no servidor
 server.use(router);
 
