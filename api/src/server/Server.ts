eb618dca7bf0f950d977ebc8ad430b1ceffaccec
gustavoventieri <ventierigustavo>
2024-12-21 01:29:19 -0300

    feat: cors added

--- api/src/server/Server.ts
+++ api/src/server/Server.ts
@@ -1,10 +1,16 @@
 import "dotenv/config";
+import cors from "cors";
 import "./shared/services/YupTranslations";
 import express from "express";
 import { router } from "./routes";
 
 // Instanciando o servidor
 const server = express();
+server.use(
+  cors({
+    origin: process.env.ENABLED_CORS?.split(";") || [],
+  })
+);
 server.use(express.json());
 // Utiliando as rotas no servidor
 server.use(router);
