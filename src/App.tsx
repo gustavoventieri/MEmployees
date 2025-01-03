0fdc40ec1835f7bc97e63df8d0b1529d23f5aeb2
gustavoventieri <ventierigustavo@gmail.com>
2025-01-03 00:59:29 -0300

    feat: responsive drawer added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
 import { AppThemeProvider } from "./shared/contexts/ThemeContext";
+import { SideBar } from "./shared/components/sidebar/SideBar";
+import { DrawerProvider } from "./shared/contexts";
 export const App = () => {
   return (
     <AppThemeProvider>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
+      <DrawerProvider>
+        <BrowserRouter>
+          <SideBar>
+            <AppRoutes />
+          </SideBar>
+        </BrowserRouter>
+      </DrawerProvider>
     </AppThemeProvider>
   );
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,19 @@
 import { Button } from "@mui/material";
 import { Navigate, Route, Routes } from "react-router-dom";
-import { useAppThemeContext } from "../shared/contexts/ThemeContext";
+import { useDrawerContext } from "../shared/contexts";
 
 export const AppRoutes = () => {
-  const { toggleTheme } = useAppThemeContext();
+  const { toggleDrawerOpen } = useDrawerContext();
   return (
     <Routes>
       <Route
         path="/"
         element={
-          <Button variant="contained" color="primary" onClick={toggleTheme}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={toggleDrawerOpen}
+          >
             Switch Theme
           </Button>
         }
