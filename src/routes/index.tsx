39f8b04c6f0aaf3c2801603c054b2394458442f5
gustavoventieri <ventierigustavo@gmail.com>
2025-01-02 23:47:05 -0300

    feat: light and provider theme added

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,14 @@ import { Navigate, Route, Routes } from "react-router-dom";
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Button>Teste</Button>} />
+      <Route
+        path="/"
+        element={
+          <Button variant="contained" color="primary">
+            Teste
+          </Button>
+        }
+      />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
